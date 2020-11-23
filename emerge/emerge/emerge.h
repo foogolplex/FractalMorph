f870c05a23aa8239866058dcc0788661f78e63f9
42ly <robertwood@tuta.io>
2020-11-23 17:50:41 -0600

    Added julia and mandelbrot visualizations

--- emerge/emerge/emerge.h
+++ emerge/emerge/emerge.h
@@ -1,4 +1,10 @@
 #include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+#include <complex>
+#include <math.h>
 #pragma once
 class emerge
 {
@@ -11,6 +17,21 @@ public:
 	void clean();
 private:
 	SDL_Window* window;
+	SDL_Surface* window_surface;
+	SDL_Surface* background_image;
+	SDL_Surface* player_sprite;
+	SDL_Renderer* renderer;
+	SDL_Color colors[1000];
+	int player_x, player_y;
+	int window_width, window_height;
+	float julia_radius;
+	float constant;
 	void load_media();
+	void draw_player();
+	SDL_Surface* load_surface(std::string file_path);
+	void draw();
+	void draw_fractal(int type);
+	void draw_julia();
+	void gen_rand_colors();
 };
 
